c74e0f2e869cc36c88a86049ebde5cabdc6fb4ad
MatthewHeun <matthew.heun@me.com>
2013-11-23 10:43:08 -0500

    Moved scripts to their own folder. Created a new "clean" script and referenced it from the typeset script. Removed cleaning functions from typeset.bash.

---
+++ Manuscript/Book_take_2/scripts/clean.bash
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+# This bash script cleans all the auxilliary files created by LaTex,
+# thereby ensureing a fresh build of the book.
+
+# Invoke the script by cd'ing into the directory that contains this file.
+# Then, type "./clean.bash"
+
+# Ensure a fresh build by deleting all auxiliary files created by LaTeX.
+cd ..
+rm -f *.aux
+rm -f *.bbl
+rm -f *.blg
+rm -f *.idx
+rm -f *.ilg
+rm -f *.ind
+rm -f *.log
+rm -f *.nlo
+rm -f *.nls
+rm -f *.pdf
+rm -f *.synctex.gz
+rm -f *.toc
+rm -f *.lof
+rm -f *.lot
+
+cd Part_0; cd Chapter_Introduction; rm -f *.aux; rm -f *.bbl; rm -f *.blg; cd ..; cd ..
+cd Part_1; cd Chapter_Materials;    rm -f *.aux; rm -f *.bbl; rm -f *.blg; cd ..; cd ..
+cd Part_2; cd Chapter_Energy;       rm -f *.aux; rm -f *.bbl; rm -f *.blg; cd ..; cd ..
+cd Part_2; cd Chapter_Embodied;     rm -f *.aux; rm -f *.bbl; rm -f *.blg; cd ..; cd ..
+cd Part_3; cd Chapter_Values;       rm -f *.aux; rm -f *.bbl; rm -f *.blg; cd ..; cd ..
+cd Part_3; cd Chapter_Intensity;    rm -f *.aux; rm -f *.bbl; rm -f *.blg; cd ..; cd ..
+cd Part_4; cd Chapter_Implications; rm -f *.aux; rm -f *.bbl; rm -f *.blg; cd ..; cd ..
+# Delete Part 5's .aux files here, if we make a Part 5.
+cd Appendix_Derivation; rm -f *.aux; cd ..
+cd Appendix_Proof_1;    rm -f *.aux; cd ..
+cd Appendix_Proof_2;    rm -f *.aux; cd ..
